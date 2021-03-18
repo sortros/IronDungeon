67cbaac4ad6369f439e293a02a37cab211c3b254
Josep <albiol957@hotmail.com>
2021-03-18 19:36:09 +0100

    en proceso del movimiento, detecta el pulsar d pero no pinta

--- js/inicio.js
+++ js/inicio.js
@@ -4,57 +4,129 @@
     
 
         const startButton = document.querySelector('#start');
-
         startButton.addEventListener('click', () => {
   
-    document.querySelector('#imgStart').classList.add('hide');
-    document.querySelector('#myCanvas').style = 'display : block';
-    drawLevel2();
-    drawPlayer();
+            document.querySelector('#imgStart').classList.add('hide');
+            document.querySelector('#start').classList.add('hide');
+            document.querySelector('#myCanvas').style = 'display : block';
+            
+            
+            
+             
+             
         })
- 
+        document.onkeydown = movement;
+        drawLevel1();
+        drawPlayer();
+        movement();
+        //clean();
+        //update();
     });
-    function drawLevel1(){
+//document.addEventListener('keydown', () => {
+//    clean();
+//    movement();
+//
+//})
+
+//class Game { 
+
+    function drawLevel1() {
         var ctx = document.querySelector('#myCanvas').getContext("2d");
-
-
-        ctx.moveTo(0, 200);
-        ctx.lineTo(600, 200);
-        ctx.stroke();
-
-
+        ctx.moveTo(0, 275);  // la posicion del personaje no puede bajar de (0, 263)
+        ctx.lineTo(600, 275);
+         ctx.stroke();
     }
-    function drawLevel2(){
-        var ctx = document.querySelector('#myCanvas').getContext("2d");
-        //this.ctx.fillStyle = 'yellow';
-        ctx.moveTo(0, 50);
-        ctx.lineTo(600, 50);
-        ctx.moveTo(0, 200);
-        ctx.lineTo(200, 200);
-        ctx.moveTo(200, 200);
-        ctx.lineTo(200, 300);
-        ctx.moveTo(200, 280);
-        ctx.lineTo(450, 280);
-        ctx.font = "18px Arial";
-        ctx.fillText("lava", 300, 300);
-        ctx.moveTo(450, 280);
-        ctx.lineTo(450, 300);
-        ctx.moveTo(450, 300);
-        ctx.lineTo(450, 200);
-        ctx.moveTo(450, 200);
-        ctx.lineTo(600, 200);
 
-        ctx.stroke();
-        
-    }
-    function drawPlayer(){ 
-       // var ctx = document.querySelector('#myCanvas').getContext("2d");
+    //function drawLevel2() {
+    //    var ctx = document.querySelector('#myCanvas').getContext("2d");
+    //    //this.ctx.fillStyle = 'yellow';
+    //    ctx.moveTo(0, 50);
+    //    ctx.lineTo(600, 50);
+    //    ctx.moveTo(0, 200);
+    //    ctx.lineTo(200, 200);
+    //    ctx.moveTo(200, 200);
+    //    ctx.lineTo(200, 300);
+    //    ctx.moveTo(200, 280);
+    //    ctx.lineTo(450, 280);
+    //    ctx.font = "18px Arial";
+    //    ctx.fillText("lava", 300, 300);
+    //    ctx.moveTo(450, 280);
+    //    ctx.lineTo(450, 300);
+    //    ctx.moveTo(450, 300);
+    //    ctx.lineTo(450, 200);
+    //    ctx.moveTo(450, 200);
+    //    ctx.lineTo(600, 200);
+    //
+    //    ctx.stroke();
+    //    
+    //}
+    function drawPlayer() { 
        // var img = document.getElementById("playerImg");
        // img.style = 'heigh : 10px' 'width';
-//
        // ctx.drawImage(img,10,10);
-
+        var ctx = document.querySelector('#myCanvas').getContext("2d");
+        //var xPos = 5;
+        //var yPos = 263;
+        //ctx.rect(xPos, yPos, 10, 10);
+        ctx.rect(5, 263, 10, 10);
+        ctx.stroke();
+       // document.onkeydown = movement;
+        //movement(e);
     }
+
+   function movement(e) {
+       //alert(e.keyCode);
+       let xPos = 5;
+       let yPos = 263;
+       if(e.keyCode === 68){ //68
+            xPos += 20;
+            //alert("hola")
+            ctx.rect(40, 263, 10, 10);
+            ctx.stroke();
+            window.requestAnimationFrame(step);
+       }
+       if(e.keyCode === 65){
+           newXPos = xPos -= 5;
+           alert("adios")
+       }
+     // if(e.keyCode === 32) {  -----------por ahora nada de saltitos-----------
+     //     for (let i=0; i<20; i++){
+     //         clean();
+     //         yPos -= 1;                  //-----salta para arriba--------
+     //         ctx.rect(xPos, yPos, 10, 10);
+     //         ctx.stroke();
+     //     }
+     //     for (let i=0; i<20; i++){
+     //         clean();
+     //         yPos += 1;                  //------salta para abajo---------
+     //         ctx.rect(xPos, yPos, 10, 10);
+     //         ctx.stroke();
+     //     }
+     //     
+     //     
+     // }
+       //clean();
+       //ctx.rect(xPos, yPos, 10, 10);
+       //ctx.stroke();
+       //window.requestAnimationFrame(step);
+   }
+
+    function clean() {
+        var ctx = document.querySelector('#myCanvas').getContext("2d");
+        ctx.clearRect(0, 0, 600, 300);
+    }
+
+    //function update() {
+    //    clean();
+    //    drawLevel1();
+    //    drawPlayer();
+    //    movement(e);
+    //    window.requestAnimationFrame(update.bind());
+    //}
+
+     
+
+//}
     
 
 
