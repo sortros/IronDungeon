63f8a8535bbd9e6a9ca3f2215e84c24e69046cba
Josep <albiol957@hotmail.com>
2021-03-19 11:33:07 +0100

    added initialScreen and transition to gameScreen

--- js/main.js
+++ js/main.js
@@ -6,9 +6,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const startButton = document.querySelector('#start');
     startButton.addEventListener('click', () => {
 
-         document.querySelector('#imgStart').classList.add('hide');
-         document.querySelector('#start').classList.add('hide');
-         document.querySelector('#myCanvas').style = 'display : block';  
+         document.querySelector('#initialScreen').style="display:none";
+         document.querySelector('#gameScreen').style="display:block";
          const ctx = canvas.getContext('2d');
          Game.start();
     })
